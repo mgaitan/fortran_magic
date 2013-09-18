720f5d6cafdb90fa8dfddcf0e4bfb64b01e8e681
Martín Gaitán <gaitan@phasety.com>
2013-09-18 19:23:20 -0300

    failing with http://dpaste.com/1386820/. Numpy bug?

--- fortranmagic.py
+++ fortranmagic.py
@@ -117,9 +117,44 @@ class FortranMagics(Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument(
         '--fcompiler',
-        # choices=allowed_fcompilers,
+        choices=allowed_fcompilers,
         help="""Specify Fortran compiler type by vendor.
-                See %%f2py --help-fcompiler to list available on your platform"""
+                See %%f2py --help-fcompiler to list available on your platform""",
+    )
+    @magic_arguments.argument(
+        '--compiler',
+        choices=allowed_compilers,
+        help="""Specify C compiler type (as defined by distutils).
+                See %%f2py --help-compiler"""
+    )
+    @magic_arguments.argument(
+        '--f90flags', help="Specify F90 compiler flags"
+    )
+    @magic_arguments.argument(
+        '--f77flags', help="Specify F77 compiler flags"
+    )
+    @magic_arguments.argument(
+        '--opt', help="Specify optimization flags"
+    )
+    @magic_arguments.argument(
+        '--arch', help="Specify architecture specific optimization flags"
+    )
+    @magic_arguments.argument(
+        '--noopt', action="store_true", help="Compile without optimization"
+    )
+    @magic_arguments.argument(
+        '--noarch', action="store_true", help="Compile without arch-dependent optimization"
+    )
+    @magic_arguments.argument(
+        '--debug', action="store_true", help="Compile with debugging information"
+    )
+    @magic_arguments.argument(
+        '--link',
+           help="""Link extension module with <resources> as defined
+                   by numpy.distutils/system_info.py. E.g. to link
+                   with optimized LAPACK libraries (vecLib on MacOSX,
+                   ATLAS elsewhere), use --link lapack_opt.
+                   See also --help-link switch."""
     )
     @cell_magic
     def fortran(self, line, cell):
@@ -143,24 +178,33 @@ class FortranMagics(Magics):
 
 
         """
+        args = magic_arguments.parse_argstring(self.fortran, line)
 
-        # args = magic_arguments.parse_argstring(self.fortran, line)
+        # boolean flags
+        f2py_args = ['--%s' % k for k, v in vars(args).items() if v is True]
 
+        if args.link:
+            print(args.link)
+
+        kw = ['--%s=%s' % (k, v) for k, v in vars(args).items()
+                          if isinstance(v, basestring)]
+
+        f2py_args.extend(kw)
         code = cell if cell.endswith('\n') else cell+'\n'
         key = code, sys.version_info, sys.executable, f2py2e.f2py_version
 
         module_name = "_fortran_magic_" + \
                       hashlib.md5(str(key).encode('utf-8')).hexdigest()
 
-        module_path = os.path.join(lib_dir, module_name + self.so_ext)
+        module_path = os.path.join(self._lib_dir, module_name + self.so_ext)
 
-        f90_file = os.path.join(lib_dir, module_name + '.f90')
+        f90_file = os.path.join(self._lib_dir, module_name + '.f90')
         f90_file = py3compat.cast_bytes_py2(f90_file,
                                             encoding=sys.getfilesystemencoding())
         with io.open(f90_file, 'w', encoding='utf-8') as f:
             f.write(code)
 
-        self._run_f2py(['-m', module_name, '-c', f90_file])
+        self._run_f2py(f2py_args + ['-m', module_name, '-c', f90_file])
 
         self._code_cache[key] = module_name
         module = imp.load_dynamic(module_name, module_path)
