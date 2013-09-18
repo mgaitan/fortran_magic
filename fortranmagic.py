8caafdcaf12f7979038a2e77ba455f832c73d3d8
Martín Gaitán <gaitan@phasety.com>
2013-09-18 16:30:36 -0300

    f2py helps line magic

--- fortranmagic.py
+++ fortranmagic.py
@@ -27,7 +27,7 @@ except ImportError:
     import md5 as hashlib
 
 from IPython.core.error import UsageError
-from IPython.core.magic import Magics, magics_class, cell_magic
+from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
 from IPython.core import display, magic_arguments
 from IPython.utils import py3compat
 from IPython.utils.io import capture_output
@@ -35,81 +35,46 @@ from IPython.utils.path import get_ipython_cache_dir
 from numpy.f2py import f2py2e
 from numpy.distutils import fcompiler
 from distutils.core import Distribution
+from distutils.ccompiler import compiler_class
 from distutils.command.build_ext import build_ext
 
 fcompiler.load_all_fcompiler_classes()
-allowed_fcompiler = fcompiler.fcompiler_class.keys()
-
 
 @magics_class
 class FortranMagics(Magics):
+
+
+    allowed_fcompilers = sorted(fcompiler.fcompiler_class.keys())
+    allowed_compilers = sorted(compiler_class.keys())
 
     def __init__(self, shell):
         super(FortranMagics, self).__init__(shell)
         self._reloads = {}
         self._code_cache = {}
+        self._lib_dir = os.path.join(get_ipython_cache_dir(), 'fortran')
+
 
     def _import_all(self, module):
         for k, v in module.__dict__.items():
             if not k.startswith('__'):
                 self.shell.push({k: v})
 
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument(
-        '--fcompiler',
-        choices=allowed_fcompiler,
-        help="Specify Fortran compiler type by vendor",
-    )
-    @cell_magic
-    def fortran(self, line, cell):
-        """Compile and import everything from a Fortran code cell, using f2py.
-
-        The contents of the cell are written to a `.f90` file in the
-        directory `IPYTHONDIR/fortran` using a filename with the hash of the
-        code. This file is then compiled. The resulting module
-        is imported and all of its symbols are injected into the user's
-        namespace.
-
-
-        Usage
-        =====
-        Prepend ``%%fortran`` to your fortran code in a cell::
-
-        ``%%fortran
-
-        ! put your code here.
-        ``
-
-
+    def _run_f2py(self, argv, show_captured=False):
         """
-        args = magic_arguments.parse_argstring(self.fortran, line)
-        print(args)
-        code = cell if cell.endswith('\n') else cell+'\n'
-        lib_dir = os.path.join(get_ipython_cache_dir(), 'fortran')
-        key = code, sys.version_info, sys.executable, f2py2e.f2py_version
-
-        if not os.path.exists(lib_dir):
-            os.makedirs(lib_dir)
-
-        module_name = "_fortran_magic_" + \
-                      hashlib.md5(str(key).encode('utf-8')).hexdigest()
-
-        module_path = os.path.join(lib_dir, module_name + self.so_ext)
-
-        f90_file = os.path.join(lib_dir, module_name + '.f90')
-        f90_file = py3compat.cast_bytes_py2(f90_file,
-                                            encoding=sys.getfilesystemencoding())
-        with io.open(f90_file, 'w', encoding='utf-8') as f:
-            f.write(code)
-
+        Here we directly call the numpy.f2py.f2py2e.run_compile() entry point,
+        after some small amount of setup to get sys.argv and the current
+        working directory set appropriately.
+        """
         old_argv = sys.argv
         old_cwd = os.getcwdu() if sys.version_info[0] == 2 else os.getcwd()
         try:
-            sys.argv = ['f2py', '-m', module_name, '-c', f90_file]
-            os.chdir(lib_dir)
+            sys.argv = ['f2py'] + argv
+            os.chdir(self._lib_dir)
             try:
                 with capture_output() as captured:
-                    f2py2e.run_compile()
+                    f2py2e.main()
+                if show_captured:
+                    captured()
             except SystemExit as e:
                 captured()
                 raise UsageError(str(e))
@@ -117,9 +82,37 @@ class FortranMagics(Magics):
             sys.argv = old_argv
             os.chdir(old_cwd)
 
-        self._code_cache[key] = module_name
-        module = imp.load_dynamic(module_name, module_path)
-        self._import_all(module)
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        '--help-link', default='all', required=False,
+        help="""List system resources found by system_info.py.
+                Optionally give a resource name.
+                E.g. try '--help-link lapack_opt.
+
+                See alsof
+                %%fortran --link <resource> switch.
+                """
+    )
+    @magic_arguments.argument(
+        '--help-fcompiler', action="store_true",
+        help="List available Fortran compilers",
+    )
+    @magic_arguments.argument(
+        '--help-compiler', action="store_true",
+        help="List available C compilers",
+    )
+    @line_magic
+    def f2py(self, line):
+        args = magic_arguments.parse_argstring(self.f2py, line)
+        if args.help_fcompiler:
+            self._run_f2py(['-c', '--help-fcompiler'], True)
+        elif args.help_compiler:
+            self._run_f2py(['-c', '--help-compiler'], True)
+        elif len(args.help_link) == 0:
+            self._run_f2py(['--help-link'])
+        elif len(args.help_link) > 0:
+            self._run_f2py(['--help-link', args.help_link], True)
+
 
     @property
     def so_ext(self):
@@ -140,7 +133,7 @@ class FortranMagics(Magics):
             self._so_ext = build_extension.get_ext_filename('')
             return self._so_ext
 
-__doc__ = __doc__.format(FORTRAN_DOC=' ' * 8 + FortranMagics.fortran.__doc__)
+# __doc__ = __doc__.format(FORTRAN_DOC=' ' * 8 + FortranMagics.fortran.__doc__)
 
 
 def load_ipython_extension(ip):
